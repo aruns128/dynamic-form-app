4cb5333a66e2a37bea4508bba57718cbfebed18b
Arunkumar <arunkumar.s@indiumsoft.com>
2023-09-05 17:11:50 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+// src/App.tsx
+
+import React from "react";
+import "./App.css";
+import DynamicForm from "./DynamicForm";
+
+const App: React.FC = () => {
+  const farmFields = [
+    { label: "First Name", fieldType: "text", fieldName: "firstName" },
+    { label: "Last Name", fieldType: "text", fieldName: "lastName" },
+    {
+      label: "Gender",
+      fieldType: "radio",
+      fieldName: "gender",
+      options: ["Male", "Female"],
+    },
+    {
+      label: "Crops",
+      fieldType: "checkbox",
+      fieldName: "crops",
+      options: ["Wheat", "Corn", "Rice", "Barley"],
+    },
+    {
+      label: "Country",
+      fieldType: "select",
+      fieldName: "country",
+      options: ["USA", "Canada", "UK", "Australia"],
+    },
+    { label: "Email", fieldType: "email", fieldName: "email" },
+    { label: "Phone Number", fieldType: "tel", fieldName: "phoneNumber" },
+    { label: "Date of Birth", fieldType: "date", fieldName: "dateOfBirth" },
+  ];
+
+  const handleFarmCreation = (formData: Record<string, any>) => {
+    console.log("Farm Created:", formData);
+  };
+
+  return (
+    <div className="App">
+      <h1 style={{ textAlign: "center" }}>Create a Farm</h1>
+      <DynamicForm fields={farmFields} onSubmit={handleFarmCreation} />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/DynamicForm.tsx
@@ -0,0 +1,169 @@
+// src/components/DynamicFarmForm.tsx
+
+import React, { useState } from "react";
+
+interface FarmField {
+  label: string;
+  fieldType: string;
+  fieldName: string;
+  options?: string[]; // For select field
+}
+
+interface DynamicFormProps {
+  fields: FarmField[];
+  onSubmit: (formData: Record<string, any>) => void;
+}
+
+const DynamicFarmForm: React.FC<DynamicFormProps> = ({ fields, onSubmit }) => {
+  const [formData, setFormData] = useState<Record<string, any>>({});
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const { name, value } = e.target;
+
+    if (e.target.type === "checkbox" && e.target instanceof HTMLInputElement) {
+      // Handle checkbox differently for HTMLInputElement
+      const currentValue = formData[name] || [];
+      const updatedValue = e.target.checked
+        ? [...currentValue, value]
+        : currentValue.filter((item: string) => item !== value);
+
+      setFormData({ ...formData, [name]: updatedValue });
+    } else {
+      setFormData({ ...formData, [name]: value });
+    }
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    onSubmit(formData);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="container">
+        {fields.map((field) => (
+          <div key={field.fieldName} className="mb-3">
+            <label className="form-label">{field.label}:</label>
+            {field.fieldType === "text" && (
+              <div className="mb-3">
+                <input
+                  type="text"
+                  className="form-control"
+                  name={field.fieldName}
+                  value={formData[field.fieldName] || ""}
+                  onChange={handleInputChange}
+                />
+              </div>
+            )}
+            {field.fieldType === "email" && (
+              <div className="mb-3">
+                <input
+                  type="email"
+                  className="form-control"
+                  name={field.fieldName}
+                  value={formData[field.fieldName] || ""}
+                  onChange={handleInputChange}
+                />
+              </div>
+            )}
+            {field.fieldType === "password" && (
+              <div className="mb-3">
+                <input
+                  type="password"
+                  className="form-control"
+                  name={field.fieldName}
+                  value={formData[field.fieldName] || ""}
+                  onChange={handleInputChange}
+                />
+              </div>
+            )}
+            {field.fieldType === "radio" && (
+              <div className="mb-3">
+                {field.options?.map((option) => (
+                  <div key={option} className="form-check form-check-inline">
+                    <input
+                      type="radio"
+                      name={field.fieldName}
+                      value={option}
+                      checked={formData[field.fieldName] === option}
+                      onChange={handleInputChange}
+                      id={`${field.fieldName}-${option}`}
+                    />
+                    <label className="form-check-label" htmlFor={`${field.fieldName}-${option}`}>
+                      {option}
+                    </label>
+                  </div>
+                ))}
+              </div>
+            )}
+            {field.fieldType === "checkbox" && (
+              <div className="mb-3">
+                {field.options?.map((option) => (
+                  <div key={option} className="form-check form-check-inline">
+                    <input
+                      type="checkbox"
+                      name={field.fieldName}
+                      value={option}
+                      checked={formData[field.fieldName]?.includes(option) || false}
+                      onChange={handleInputChange}
+                      id={`${field.fieldName}-${option}`} // Add an ID for the input
+                    />
+                    <label className="form-check-label" htmlFor={`${field.fieldName}-${option}`}>
+                      {option}
+                    </label>
+                  </div>
+                ))}
+              </div>
+            )}
+            {field.fieldType === "select" && (
+              <div className="mb-3">
+                <select
+                  className="form-select"
+                  name={field.fieldName}
+                  value={formData[field.fieldName] || ""}
+                  onChange={handleInputChange}
+                >
+                  <option value="">Select {field.label}</option>
+                  {field.options?.map((option) => (
+                    <option key={option} value={option}>
+                      {option}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            )}
+            {field.fieldType === "date" && (
+              <div className="mb-3">
+                <input
+                  type="date"
+                  className="form-control"
+                  name={field.fieldName}
+                  value={formData[field.fieldName] || ""}
+                  onChange={handleInputChange}
+                />
+              </div>
+            )}
+            {field.fieldType === "tel" && (
+              <div className="mb-3">
+                <input
+                  type="tel"
+                  className="form-control"
+                  name={field.fieldName}
+                  value={formData[field.fieldName] || ""}
+                  onChange={handleInputChange}
+                />
+              </div>
+            )}
+          </div>
+        ))}
+        <div className="text-align pt-10">
+          <button type="submit" className="btn btn-primary">
+            Create Farm
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default DynamicFarmForm;
